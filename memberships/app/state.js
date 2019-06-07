0812629117cad989acb5b43e41077e49c7df9d92
Goodwish Matyila <goodwish@axxess.co.za>
2019-06-07 17:18:46 +0200

    beta version release

--- memberships/app/state.js
+++ memberships/app/state.js
@@ -18,7 +18,15 @@ let dashboardProps = kendo.observable({
                     Surname:dashboardProps.get("profile.Surname"),
                     Phone:dashboardProps.get("profile.Phone"),
                     Email: user.email
-                  }) 
+                  })
+                  
+                  .catch(function(error){
+                    kendo.confirm("Kindly check your fields again");
+                  });
+
+                  setTimeout(function(){
+                    location.replace("./index.html");
+                  },3000);
               }
             })
           }
@@ -55,12 +63,6 @@ let dashboardProps = kendo.observable({
                userProfileStored.get("activeMembers").slice([]);
 
                userProfileStored.get("activeMembers").push(client.data());
-               
-                console.log(userProfileStored.get("activeMembers"));
-                
-               
-             
-
              });
             userDefined.catch(function(error){
               kendo.alert("kindly check your internet connectivity or speed");
@@ -81,41 +83,27 @@ let dashboardProps = kendo.observable({
           dashboardProps.set("profile.Email", Email );
           dashboardProps.set("profile.Names", Names);
           dashboardProps.set("profile.Surname", Surname);
-          dashboardProps.set("profile.Phone", Surname);
+          dashboardProps.set("profile.Phone", Phone);
       });
 
       customer.catch(function(error){
         kendo.confirm("Kindly complete your CFT Profile")
       })
-   
     }
   })
 }())
 
 dataSynchronization();
-
-
-
-
-  
-
-
-
-
-
-   
-  
+ 
   firebase.auth().onAuthStateChanged(function(user){
       if(user){
         dashboardProps.set("profile.Email", user.email);
-
- 
       }else{
           location.replace("../index.html");
       }
   })
 
- 
+
   kendo.bind(document.body, dashboardProps);
 
   let Layout = new kendo.Layout("ui-layout");
@@ -144,14 +132,13 @@ dataSynchronization();
       dataSource: userProfileStored.activeMembers,
       sortable: true,
       pageSize: 5,
-      filterable: true
+      filterable: true,
+      selectable:true
     });
-
-    userProfileStored.sync();
-
   })
 
   Router.start();
 }
+
 
 $(document).ready(init.bind(this)); 
